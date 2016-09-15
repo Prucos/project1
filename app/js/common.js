fd1a51e094c89058c91543978cde7c56a52f86b4
Chibas <vesel22@gmail.com>
2016-09-16 02:52:36 +0300

    Website header ready
    Header is ready, need to solve a couple of things with resonsivness

---
+++ app/js/common.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+  $('.fa-bars').on('click', function(){
+    toggleMenu('nav', 'opened');
+  });
+
+  $('nav ul li a').on('click', function(){
+    $('nav ul li').removeClass('active');
+    $(this).parent().addClass('active');
+   });
+
+   $('.box').on('click', function(){
+     $('.box').removeClass('active');
+     $(this).addClass('active');
+    });
+
+  function toggleMenu(toggler, togClass) {
+    if ($(toggler).hasClass(togClass)) {
+        $(toggler).removeClass(togClass);
+        $(toggler).slideUp(300);
+    } else {
+      $(toggler).slideDown(300).addClass(togClass);
+    }
+  };
+
+
+});
